6f4c0eafc45f1dbf7004a3d57dde3b30395890c5
LokeshNaidu737 <lokesh@maxzen.tech>
2026-04-07 19:24:10 +0530

    Delete src directory

--- src/App.test.skip.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders loading screen", () => {
-  render(<App />);
-  const loadingText = screen.getByText(/loading buses/i);
-  expect(loadingText).toBeInTheDocument();
-});
